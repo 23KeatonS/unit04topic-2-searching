a42a23a8037da6071d54664aeae5e8af8fdcc3b5
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-04-03 12:29:06 -0500

    done i think

--- StringListSearch.java
+++ StringListSearch.java
@@ -3,7 +3,46 @@ import java.util.ArrayList;
 public class StringListSearch {
 
     public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+        for(int i = 0;i<items.size();i++){
+            if(items.get(i).equals(term)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int orderedLinearSearch(ArrayList<String> items, String term){
+        for(int i =0;i<items.size();i++){
+            if(items.get(i).compareTo(term)>0){
+                return -1;
+            }
+            if(items.get(i).equals(term)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    //is, the,there, mountain
+    public static int binarySearch(ArrayList<String> items, String term){
+        int maxIndex = items.size()-1;
+        int minIndex = 0;
+        int mid = (minIndex+maxIndex)/2;
+        while(minIndex!=maxIndex){
+            if(items.get(mid).equals(term)){
+                return mid;
+            }
+            if(items.get(mid).compareTo(term)<0){
+                minIndex = mid+1;
+            }
+            if(items.get(mid).compareTo(term)>0){
+                maxIndex = mid-1;
+            }
+            mid = (maxIndex+minIndex)/2;
+        }
+        if(items.get(mid).equals(term)){
+            return mid;
+        }
+        return -1;
     }
     
     public static void main(String[] args) {
--- StringListSearchTest.java
+++ StringListSearchTest.java
@@ -19,5 +19,93 @@ public class StringListSearchTest {
         expectedOutput = 2;
         actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
         assertEquals(expectedOutput, actualOutput);
+
+
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "there";
+        expectedOutput = 0;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "valley";
+        expectedOutput = -1;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        inputTerm = "mountain";
+        expectedOutput = 3;
+        actualOutput = StringListSearch.unorderedSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testOrderedLinearSearch() {
+        ArrayList<String> inputItems;
+        String inputTerm;
+        int expectedOutput;
+        int actualOutput;
+
+        // some positive numbers
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "the";
+        expectedOutput = 1;
+        actualOutput = StringListSearch.orderedLinearSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "there";
+        expectedOutput = 2;
+        actualOutput = StringListSearch.orderedLinearSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "valley";
+        expectedOutput = -1;
+        actualOutput = StringListSearch.orderedLinearSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "mountain";
+        expectedOutput = 0;
+        actualOutput = StringListSearch.orderedLinearSearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testBinarySearch() {
+        ArrayList<String> inputItems;
+        String inputTerm;
+        int expectedOutput;
+        int actualOutput;
+
+        // some positive numbers
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "the";
+        expectedOutput = 1;
+        actualOutput = StringListSearch.binarySearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+
+
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "there";
+        expectedOutput = 2;
+        actualOutput = StringListSearch.binarySearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+        
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "valley";
+        expectedOutput = -1;
+        actualOutput = StringListSearch.binarySearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
+        
+
+        inputItems = new ArrayList<>(Arrays.asList("mountain","the","there","is"));
+        inputTerm = "mountain";
+        expectedOutput = 0;
+        actualOutput = StringListSearch.binarySearch(inputItems, inputTerm);
+        assertEquals(expectedOutput, actualOutput);
     }
 }
